5ec9e4b564d77ede4a963e2589e6db2316e28cd4
Mahmoud Taghizadeh <mtirenji@gmail.com>
2026-07-30 03:29:03 +0300

    remove

--- data/.gitkeep.ps1
+++
@@ -1,5 +0,0 @@
-# This file ensures the data directory is tracked in git
-@"
-# This directory contains the database file
-# db.json will be created automatically when the server starts
-"@ | Out-File -FilePath data\.gitkeep -Encoding utf8
